d707455dd6cb307f317788af4e6c6c791ac38c1f
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-11 17:43:37 -0400

    Add event model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,6 +31,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    session[:username] = nil
+
     redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_153258) do
+ActiveRecord::Schema.define(version: 2020_06_11_213950) do
+
+  create_table "events", force: :cascade do |t|
+    t.date "event_date"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
