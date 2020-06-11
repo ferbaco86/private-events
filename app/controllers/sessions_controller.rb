d19e7581c4e522cbccabb61e1423dc2ff75d7885
LoboArkano <pipiolo_mananes@hotmail.com>
2020-06-11 15:52:18 -0500

    Update create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,30 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new 
+    # @user = User.new
   end
+
   def create
-      @user = User.find_by(params[:name])
-      session[:current_user_id] = @user.id
-      if session[:current_user_id] != nil
-        redirect_to users_path
+      @user_temp = User.find_by(name: params[:username_login])
+
+      
+      if @user_temp
+        session[:user_id] = @user_temp.id
+        render json: {
+          status: :created,
+          logged_in: true,
+          user: @user_temp
+        }
+      else
+        render json: { status: 401 }
       end
   end
   
   def log_in
-    @user = User.find_by(params[:name])
-
+    @user = User.new
   end
 
   def destroy
+    session[:user_id] = nil
   end
 end
