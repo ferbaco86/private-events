a51040bf12e199c9fd0538b40c9cd36da8f5d815
LoboArkano <pipiolo_mananes@hotmail.com>
2020-06-11 12:02:29 -0500

    Create login and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def login
+    @user = User.find_by(params[:name])
+    session[:current_user_id] = @user.id
+    if session[:current_user_id] != nil
+      render :show
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
   get 'users/show'
+  get 'users/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[new create show]
+  resources :users, only: %i[new create show login]
   root "users#new"
 end
