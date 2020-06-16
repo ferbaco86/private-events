3acca421caf0371d0e9f3d17832d6f087646ba05
LoboArkano <pipiolo_mananes@hotmail.com>
2020-06-16 15:42:26 -0500

    Add test for associations

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  context 'creating event' do
+    it 'description cannot be blank ' do   #
+      expect{ Event.create!}.to raise_error(ActiveRecord::RecordInvalid)  # test code
+    end
+  end
+
+  context 'testing associations' do
+    it "should have many users" do
+      e = Event.reflect_on_association(:creator).macro
+      expect(e).to eq(:belongs_to)
+    end
+  end
+end
