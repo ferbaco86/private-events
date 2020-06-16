919d73a77663a4a8d777d2112df3ee88c86550d4
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-16 12:29:17 -0400

    Update previous and upcoming events methods on User model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User"
-  has_many :attendees, through: :attendances, source: :event_attendee
   has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendances, source: :event_attendee
+  belongs_to :creator, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
-  has_many :events, foreign_key: :creator_id, class_name: "Event"
-  has_many :attended_event, through: :attendances
   has_many :attendances, foreign_key: :event_attendee_id
+  has_many :attended_event, through: :attendances
+  has_many :events, foreign_key: :creator_id, class_name: "Event"
 
   def upcoming_events
     time = Time.now
-    @upcoming_events = Event.select(:id,:event_date,:location,:description).where("event_date > ?", time.to_formatted_s(:db))
+    self.attended_event.select(:id,:event_date,:location,:description).where("event_date > ?", time.to_formatted_s(:db))
   end
 
   def previous_events
     time = Time.now
-    @previous_events = Event.select(:id,:event_date,:location,:description).where("event_date <= ?", time.to_formatted_s(:db))
+    self.attended_event.select(:id,:event_date,:location,:description).where("event_date <= ?", time.to_formatted_s(:db))
   end
 end
