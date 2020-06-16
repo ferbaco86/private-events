29ede45614dfa5c36280777a7d0aac29913f5ba2
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-16 18:00:32 -0400

    Add tests for create user, login and create events

--- spec/views/user_spec.rb
+++ spec/views/user_spec.rb
@@ -1,16 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe "the signin process", type: :feature do
-  before :each do
-    User.make(name: 'Vader')
-  end
-
-  it "signs me in" do
-    visit '/users/new'
-    within("#users") do
-      fill_in 'name', with: 'vader'
+RSpec.describe 'Webpage workflow', type: :system do
+  describe 'Create new user' do
+    it 'creates a new user' do
+      visit new_user_path
+      fill_in 'name', with: 'Luke'
+      click_button 'Create User'
+      sleep(5)
     end
-    click_button 'Create User'
-    expect(page).to have_content 'Success'
+  end
+  User.create(:name=>'Luke')
+  describe 'Login user' do
+    it 'Logs in the user to the webpage' do
+      visit "/sessions/log_in"
+      fill_in 'username_login', with: 'Luke'
+      click_button 'Login'
+      sleep(5)
+    end
+  end
+  describe 'Create Event' do
+    User.create(:name => 'Luke')
+    it 'creates a new event' do
+      visit new_event_path
+      fill_in 'description', with: 'Testing views'
+      click_button 'Create Event'
+      sleep(5)
+    end
   end
 end
+
+# RSpec.describe "the signin process", type: :system do
+#   before :each do
+#     User.make(name: 'Vader')
+#   end
+
+#   it "signs me in" do
+#     visit '/users/new'
+#     within("#users") do
+#       fill_in 'name', with: 'vader'
+#     end
+#     click_button 'Create User'
+#     expect(page).to have_content 'Success'
+#   end
+#end
