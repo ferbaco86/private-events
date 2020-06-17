0a4dac90c26b084a308dd6bdb43e5dd7c3e58214
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-17 11:47:48 -0400

    Add new webdrivers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,11 +5,14 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'webdrivers/chromedriver'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
+
+
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -18,5 +21,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
   Capybara.default_driver = :selenium_chrome
 end
